934c24727622a6d37da07db422cf2b5fa6b423b5
Glib Skrypka <gsk@glibs-macbook-air.home>
2023-07-31 11:23:45 +0200

    new features

--- main.py
+++ main.py
@@ -34,6 +34,10 @@ from typing import Optional
 
 with st.sidebar:
     openai_api_key = st.text_input ("Open AI API key", type="password", key="OPENAI_API_KEY")
+    model = st.selectbox(
+        'Choose Open AI Model (GPT-4 is recommended)',
+        ('gpt-4', 'gpt-3.5-turbo')
+    )
     eleven_labs_api_key = st.text_input ("Eleven labs API key", type="password", key="ELEVENLABS_API_KEY")
 
 
@@ -79,55 +83,65 @@ meditation_length = st.selectbox(
      ('Short <5 min', 'medium 5-10 min', 'long >10 min')
 )
 
+language= st.selectbox(
+    'Select language of meditation',
+     ('English', 'Spanish', 'French', 'Hindi', 'Italian', 'German', 'Polish', 'Portuguese')
+)
+
 background_sound_type = st.selectbox(
     'Select background sounds',
      ('forest', 'rain')
 )
 
 if st.button("Generate"):
-    # Define llm
-    llm = ChatOpenAI(openai_api_key=openai_api_key, model="gpt-4", temperature=0.7)
-    llm_chain = create_structured_output_chain(Sections, llm=llm, prompt=chat_prompt)
-    with st.spinner("Generating meditation..."):
+    if eleven_labs_api_key is None or openai_api_key is None:
+        st.error("Please enter API keys")
+        st.stop()
+    else:
+        # Define llm
+        llm = ChatOpenAI(openai_api_key=openai_api_key, model=model, temperature=0.7)
+        llm_chain = create_structured_output_chain(Sections, llm=llm, prompt=chat_prompt)
+        with st.spinner("Generating meditation..."):
 
-        # Get meditation value from data dictionary based on selected meditation type
-        desc = meditation_type + ": " + data.get(meditation_type, "No meditation type selected")
-        response = llm_chain.run({"type": meditation_type, "desc": desc, "goal": goal, "duration": meditation_length})
+            # Get meditation value from data dictionary based on selected meditation type
+            desc = meditation_type + ": " + data.get(meditation_type, "No meditation type selected")
+            response = llm_chain.run({"type": meditation_type, "desc": desc, "goal": goal, "duration": meditation_length, "lang": language})
 
-    for Section in response.sections:
-        st.write(Section.text)
-        st.write(Section.pause)
+        with st.expander("see script"):
+            for Section in response.sections:
+                st.write(Section.text)
+                st.write(Section.pause)
 
-    with st.spinner("Generating audio..."):
-        voice_over = AudioSegment.empty()
-        for Section in response.sections:
+        with st.spinner("Generating audio..."):
+            voice_over = AudioSegment.empty()
+            for Section in response.sections:
 
-            audio = generate(
-                    api_key=eleven_labs_api_key,
-                    text=Section.text,
-                    voice="XB0fDUnXU5powFXDhCwa",
-                    model='eleven_multilingual_v1'
-                    )
-        
-            audio_file = BytesIO(audio)
-            audio_proc = AudioSegment.from_file(audio_file, format="mp3")
-            voice_over += audio_proc
-            pause_timing = Section.pause * 1000
-            pause = AudioSegment.silent(duration=pause_timing)
-            voice_over += pause
+                audio = generate(
+                        api_key=eleven_labs_api_key,
+                        text=Section.text,
+                        voice="XB0fDUnXU5powFXDhCwa",
+                        model='eleven_multilingual_v1'
+                        )
             
-        # Create background sound path which consist of "sounds" folder + background_sound_type + ".mp3"
-        background_sound_path = "sounds/" + background_sound_type + ".mp3"
-        # Create overlay
-        background = AudioSegment.from_file(background_sound_path, format="mp3")
-        meditation = voice_over.overlay(background, loop=True)
-        
-    # Creating audio file
-    meditation_file = meditation.export("meditation.mp3", format="mp3")
-    audio_file = open("meditation.mp3", "rb")
-    audio_bytes = audio_file.read()
+                audio_file = BytesIO(audio)
+                audio_proc = AudioSegment.from_file(audio_file, format="mp3")
+                voice_over += audio_proc
+                pause_timing = Section.pause * 1000
+                pause = AudioSegment.silent(duration=pause_timing)
+                voice_over += pause
+                
+            # Create background sound path which consist of "sounds" folder + background_sound_type + ".mp3"
+            background_sound_path = "sounds/" + background_sound_type + ".mp3"
+            # Create overlay
+            background = AudioSegment.from_file(background_sound_path, format="mp3")
+            meditation = voice_over.overlay(background, loop=True)
+            
+        # Creating audio file
+        meditation_file = meditation.export("meditation.mp3", format="mp3")
+        audio_file = open("meditation.mp3", "rb")
+        audio_bytes = audio_file.read()
 
-    st.audio(audio_bytes, format="audio/mp3", start_time=0)
+        st.audio(audio_bytes, format="audio/mp3", start_time=0)
 
         
     
